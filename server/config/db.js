3fbf1366036200b002ec389fc35140d1aa0c6e73
Titah Baudwin <baudwin12@gmail.com>
2023-11-16 15:28:11 +0100

    latest commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const mysql = require("mysql2")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const db = mysql.createPool({
     host: "127.0.0.1",
     port: "3306",
     database: "books",
     user: "root",
-    password: "Michealtutu123"
+    password: process.env.password
 
 }).promise()
 
